47cd0504cbb57bfc117780f2556f6c1ea9abbedd
busynest <jack.solutionbox@gmail.com>
2018-03-22 00:31:32 -0700

    March 21 2018

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,42 @@
 
 const {resolve, join} = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 const pkg = require('./package.json');
 
-const moduleConf = require('./webpack-module.config');
-const nomoduleConf = require('./webpack-nomodule.config');
+const config = {
 
-const ENV = process.argv.find(arg => arg.includes('NODE_ENV=production')) ? 'production' : 'development';
-const IS_DEV_SERVER = process.argv.find(arg => arg.includes('webpack-dev-server'));
-const OUTPUT_PATH = IS_DEV_SERVER ? resolve('src') : resolve('dist');
+  mode: 'production',
 
-const processEnv = {
-  NODE_ENV: JSON.stringify(ENV),
-  appVersion: JSON.stringify(pkg.version)
+  output: {
+    filename: 'roofers.js'
+  },
+
+  module: {
+    rules: [
+
+      {
+        test: /\.html$/,
+        use: ['text-loader']
+      },
+
+      {
+        test: /\.js$/,
+        use: ['babel-loader']
+      }
+
+    ]
+  },
+
+
 };
 
+module.exports = (env = {}) => merge(env.BROWSERS === 'module' ? moduleConf() : nomoduleConf(), shared(env));
+
 /**
- * === Copy static files configuration
- */
-const copyStatics = {
-  copyWebcomponents: [{
+  const outputPath = resolve('dist');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
+    copyWebcomponents: [{
     from: resolve('./node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js'),
     to: join(OUTPUT_PATH, 'vendor'),
     flatten: true
@@ -90,60 +103,4 @@ const copyStatics = {
     to: OUTPUT_PATH,
     flatten: true
   }]
-};
-
-/**
- * Plugin configuration
- */
-const plugins = [IS_DEV_SERVER ?
-  new CopyWebpackPlugin(copyStatics.copyWebcomponents) :
-  new WorkboxPlugin({
-    globDirectory: OUTPUT_PATH,
-    globPatterns: ['**/!(*map*)'],
-    globIgnores: ['**/sw.js'],
-    swDest: join(OUTPUT_PATH, 'sw.js')
-  }),
-  new CopyWebpackPlugin(
-    [].concat(copyStatics.copyWebcomponents, copyStatics.copyOthers)
-  )
-].concat([
-  new webpack.DefinePlugin({'process.env': processEnv})
-]);
-
-const shared = env => {
-  const IS_MODULE_BUILD = env.BROWSERS === 'module';
-
-  return {
-    entry: './src/index.js',
-    devtool: 'cheap-module-source-map',
-    output: {
-      path: OUTPUT_PATH,
-      filename: IS_MODULE_BUILD ? 'module.bundle.js' : 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.html$/,
-          use: ['text-loader']
-        },
-        {
-          test: /\.pcss$/,
-          use: ['text-loader', 'postcss-loader']
-        }
-      ]
-    },
-    plugins,
-    devServer: {
-      contentBase: OUTPUT_PATH,
-      compress: true,
-      overlay: {
-        errors: true
-      },
-      port: 3000,
-      host: '0.0.0.0',
-      disableHostCheck: true
-    }
-  };
-};
-
-module.exports = (env = {}) => merge(env.BROWSERS === 'module' ? moduleConf() : nomoduleConf(), shared(env));
+ * */
